4ea1a071dc1e1e25333b71faee5e3e1bb9b53c9f
steelworx <eileencuevas62@gmail.com>
2019-04-10 22:02:45 -0400

    added code for queues

---
+++ projects/adventure/queue.py
@@ -0,0 +1,19 @@
+class Queue:
+    def __init__(self):
+        self.queue_size = 0
+        self.storage = []
+
+    def enqueue(self, item):
+        self.queue_size += 1
+        self.storage.append(item)
+
+    def dequeue(self):
+        if self.queue_size > 0:
+            self.queue_size -= 1
+            return self.storage.pop(0)
+
+    def size(self):
+        return self.queue_size
+
+    def isEmpty(self):
+        return self.storage == []
